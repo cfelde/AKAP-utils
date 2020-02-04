9a5d736c0c382caa39ddaa6733bc9b3c8f9e6cc8
Christian Felde <cfelde@cfelde.com>
2020-02-04 18:33:49 +0000

    Use hex like elsewhere

--- contracts/types/Uint256Lib.sol
+++ contracts/types/Uint256Lib.sol
@@ -16,9 +16,9 @@ pragma solidity ^0.5.0;
 
 library Uint256Lib {
     function asBytes(uint256 x) internal pure returns (bytes memory b) {
-        b = new bytes(32);
+        b = new bytes(0x20);
         assembly {
-            mstore(add(b, 32), x)
+            mstore(add(b, 0x20), x)
         }
     }
 }
