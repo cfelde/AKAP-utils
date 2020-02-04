7213ecbe3a730c58a1fe56e044c30c1e7fd374a7
Christian Felde <cfelde@cfelde.com>
2020-02-04 19:09:17 +0000

    Uint8 lib and utils

--- contracts/types/BytesLib.sol
+++ contracts/types/BytesLib.sol
@@ -19,6 +19,16 @@ library BytesLib {
         return string(b);
     }
 
+    function asUint8(bytes memory b) internal pure returns (uint8 x) {
+        if (b.length == 0) {
+            x = 0;
+        } else {
+            assembly {
+                x := mload(add(b, 0x1))
+            }
+        }
+    }
+
     function asUint256(bytes memory b) internal pure returns (uint256 x) {
         if (b.length == 0) {
             x = 0;
